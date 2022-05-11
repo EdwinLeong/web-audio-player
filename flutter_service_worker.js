f8e8ed34021941b122827b2bfb67259aa4aff040
Zhi Yi Leong <zhiyi@revenuemonster.my>
2022-05-11 23:49:08 +0800

    feat: add animation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e6ceaca8e3c3053230d96ab4211f1ccc",
-"index.html": "765789901a9033e956ef356d346d5bb5",
-"/": "765789901a9033e956ef356d346d5bb5",
-"main.dart.js": "f0de2f85990f86eec1ec9da6186c2288",
+"index.html": "e88e150d2b22fcab811c7884720eb61b",
+"/": "e88e150d2b22fcab811c7884720eb61b",
+"main.dart.js": "0b21dd494995750b75ec39adc2680ce4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
